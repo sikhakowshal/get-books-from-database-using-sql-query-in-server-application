1e2c3ca2e4b400f5a28a414bc552132f9b45eeff
Sikha Kowshal <koushalsikha4@gmail.com>
2022-12-25 18:51:45 +0000

    final-code

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (error) {
+    console.log(`DB Error : ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+        SELECT *
+        FROM book
+        ORDER BY book_id;
+    `;
+
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
